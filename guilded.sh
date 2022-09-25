2239e5c47df4f27115ef8204a2db270dcac1d5d4
losuler <losuler@posteo.net>
2022-09-26 09:31:03 +1000

    Fix Nvidia rendering on Xwayland
    Based on https://github.com/flathub/com.discordapp.Discord/commit/df23add89b9af3c84414a7a145ac272d7469df27

--- guilded.sh
+++ guilded.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/extra/guilded/guilded "$@"
+
+if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -f /dev/nvidia0 ]
+then
+  FLAGS="$FLAGS --disable-gpu-sandbox"
+fi
+
+env TMPDIR="$XDG_CACHE_HOME" zypak-wrapper /app/extra/guilded/guilded "$FLAGS" "$@"
